0e5a84b3969cfff5225022da4788f65f792f9d6c
Tess Ferrandez <tess.ferrandez@microsoft.com>
2021-05-05 09:49:32 +0200

    update terraform scripts

--- tf/main.tf
+++ tf/main.tf
@@ -18,3 +18,28 @@ resource "azurerm_app_service_plan" "api_service_plan" {
     tier = "Free"
   }
 }
+
+resource "azurerm_app_service" "api_app_service" {
+  app_service_plan_id =  "/subscriptions/47c53038-ea3d-4516-b6e8-875e26227fed/resourceGroups/fastapi-rg/providers/Microsoft.Web/serverfarms/treapi-1234"
+  app_settings = {"SCM_DO_BUILD_DURING_DEPLOYMENT" = "1"}
+  # client_affinity_enabled = false
+  # client_cert_enabled = false
+  # enabled = true
+  https_only = false
+  location = "westeurope"
+  name = "treapi1234"
+  resource_group_name = "fastapi-rg"
+  site_config {
+    always_on = false
+    app_command_line = "gunicorn -w 4 -k uvicorn.workers.UvicornWorker api.main:app"
+    linux_fx_version = "PYTHON|3.8"
+    # managed_pipeline_mode = "Integrated"
+    # min_tls_version = "1.2"
+    # number_of_workers = 1
+    use_32_bit_worker_process = true
+  }
+  source_control {
+    branch             = "main"
+    repo_url           = "https://github.com/TessFerrandez/tre_api"
+  }
+}
